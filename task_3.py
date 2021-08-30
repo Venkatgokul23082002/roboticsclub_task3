e9c3ce90089a312afde88d5e9291ce52c9f9e2b3
VENKATA KRISHNAN A <85434970+Venkatgokul23082002@users.noreply.github.com>
2021-08-30 12:59:18 -0700

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,7 +1,15 @@
 #############################
 # import necessary libraries
 #############################
-
+import cv2
+import cv2.aruco
+import matplotlib.pyplot as plt
+import imutils
+import numpy as np
+from google.colab.patches import cv2_imshow
+import time
+import matplotlib
+import os
 
 ####################################################################
 def get_frame(time_sec):
@@ -18,7 +26,13 @@ def get_frame(time_sec):
         frame at the given second
     '''
     ##############	ADD YOUR CODE HERE	##############
-
+time=input()
+cap = cv2.VideoCapture('./aruco_bot.mp4')
+fps = cap.get(cv2.CAP_PROP_FPS) 
+frame_seq = int(time)*fps
+cap.set(1, frame_seq)
+ret, frame = cap.read()  
+cap.release()
     ##################################################
     frame = ''
     return frame
@@ -39,6 +53,13 @@ def detect_Aruco(frame):
     '''
     ##############	ADD YOUR CODE HERE	##############
 
+alpha = float(2.2)
+beta = int(45)
+frame = np.clip(alpha*frame + beta, 0, 255)
+gray=cv2.cvtColor(frame.astype('float32'),cv2.COLOR_BGR2GRAY)
+arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_5X5_50)
+arucoParams = cv2.aruco.DetectorParameters_create()
+corners, ids, rejected = aruco.detectMarkers(gray.astype('uint8'), arucoDict, parameters = arucoParam)
     ##################################################
     corners = []
     return corners
@@ -62,6 +83,15 @@ def mark_Aruco(frame, corners):
     '''
     ##############	ADD YOUR CODE HERE	##############
 
+x_sum = corners[0][0][0][0]+ corners[0][0][1][0]+ corners[0][0][2][0]+ corners[0][0][3][0]
+y_sum = corners[0][0][0][1]+ corners[0][0][1][1]+ corners[0][0][2][1]+ corners[0][0][3][1]  
+cX= x_sum*0.25
+cY = y_sum*0.25
+cv2.circle(frame, (int(cX),int( cY)), 4, (0, 0, 255), -1)
+cv2.putText(frame, str(ids[0][0]),(int(cX)+3,int(cY)), cv2.FONT_HERSHEY_SIMPLEX,0.5, (0, 255, 0), 2)
+for i in range(0,4):
+  cv2.circle(frame, (corners[0][0][i][0],corners[0][0][i][1]), 4, (0, 0, 255), -1)
+cv2_imshow(frame)
     ##################################################
     marked_frame = ''
     return marked_frame
